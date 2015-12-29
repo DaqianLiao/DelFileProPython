fba9142bdea5dceccdacf1b93c4cbb8460c7e5ac
DaqianLiao <360989637@qq.com>
2015-12-29 11:03:49 +0800

    init file

---
+++ delete_file/delete_path_all_txtfile.py
@@ -0,0 +1,33 @@
+import gzip
+import os.path
+
+#解压指定路径下的所有文件
+def  delete_xls_all(log_path):
+    for dirpath, dirnames, filenames in os.walk(log_path):
+        for filename in filenames:
+            if os.path.splitext(filename)[1] == '.txt':
+               filepath = os.path.join(dirpath, filename)
+               delfile(filepath) # 删除文件
+               print(filepath, ' : this file is deleted!')
+
+#删除单个文件          
+def delfile(fname):
+    if os.path.exists(fname):
+        os.remove(fname)
+
+               
+def main():
+    log_path=input('enter a path:')
+    if os.path.exists(log_path):#判断路径是否存在 异常处理1
+       delete_xls_all(log_path)
+    else:
+        print('path do not exist')
+        main()
+
+if __name__ == "__main__":
+    main()
+
+               
+
+
+
