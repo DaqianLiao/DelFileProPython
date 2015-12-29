e4bcba8d678c36b6cc37a792826979e53cce4fd2
DaqianLiao <360989637@qq.com>
2015-12-29 11:53:38 +0800

    make file more standard

--- delete_file/delete_path_all_xlsfile.py
+++ delete_file/delete_path_all_xlsfile.py
@@ -1,4 +1,3 @@
-import gzip
 import os.path
 
 #解压指定路径下的所有文件
@@ -6,18 +5,15 @@ def  delete_xls_all(log_path):
     for dirpath, dirnames, filenames in os.walk(log_path):
         for filename in filenames:
             if os.path.splitext(filename)[1] == '.xls':
-##            if 'remalloc' in filename:
                filepath = os.path.join(dirpath, filename)
-               delfile(filepath) # 删除文件
+               delfile(filepath)
                print(filepath,' : this file is deleted!')
-
 
 #删除单个文件          
 def delfile(fname):
     if os.path.exists(fname):
         os.remove(fname)
-               
-               
+            
 def main():
     log_path=input('enter a path:')
     #log_path = 'E:\\python_log\\test_gz_file'
